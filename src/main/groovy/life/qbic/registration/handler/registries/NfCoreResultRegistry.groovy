467724b5c6823dfc9c7d1ff3b3995aa57dc94827
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-05-10 17:10:46 +0200

    Methods scaffold

--- src/main/groovy/life/qbic/registration/handler/registries/NfCoreResultRegistry.groovy
+++ src/main/groovy/life/qbic/registration/handler/registries/NfCoreResultRegistry.groovy
@@ -4,6 +4,9 @@ import ch.systemsx.cisd.etlserver.registrator.api.v2.IDataSetRegistrationTransac
 import life.qbic.registration.handler.RegistrationException
 import life.qbic.registration.handler.Registry
 
+import java.nio.file.Path
+import java.nio.file.Paths
+
 /**
  * <p>Registers nf-core pipeline results to openBIS.</p>
  * <br>
@@ -17,6 +20,10 @@ import life.qbic.registration.handler.Registry
  */
 class NfCoreResultRegistry implements Registry {
 
+   private enum AnalysisType {
+        VARIANT_CALLING
+   }
+
     /**
      * {@inheritDocs}
      */
@@ -24,4 +31,50 @@ class NfCoreResultRegistry implements Registry {
     void executeRegistration(IDataSetRegistrationTransactionV2 transaction) throws RegistrationException {
 
     }
+
+    /*
+    Returns the measurement sample codes used for the analysis as input.
+     */
+    private List<String> getInputSamples() {
+        parseSampleIdsFrom(Paths.get("./"))
+    }
+    /*
+    Iterates through the lines of a file and extracts the sample codes.
+    The sample codes must be line separated. All trailing whitespace will get trimmed.
+     */
+    private List<String> parseSampleIdsFrom(Path file) {
+        null
+    }
+    /*
+    Returns the unique workflow run id from Nextflow Tower.
+     */
+    private String getTowerRunId() {
+        parseRunIdFrom(Paths.get("./"))
+    }
+    /*
+    Parse the run id from the given file.
+     */
+    private String parseRunIdFrom(Path file) {
+        null
+    }
+    /*
+    Returns the analysis type.
+     */
+    private AnalysisType getAnalysisType() {
+        String pipelineName = getPipelineName()
+        determineAnalysisTypeFrom(pipelineName)
+    }
+    /*
+    Returns the pipeline name
+     */
+    private String getPipelineName() {
+        null
+    }
+    /*
+    Returns the associated analysis type based on the pipeline name.
+     */
+    private AnalysisType determineAnalysisTypeFrom(String pipelineName) {
+        null
+    }
+
 }
