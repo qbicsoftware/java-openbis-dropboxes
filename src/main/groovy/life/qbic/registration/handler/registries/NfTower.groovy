1b55741745b6f399aa913fbd97b5c27c8a10e103
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-06-21 10:34:09 +0200

    Provide some JavaDocs

--- src/main/groovy/life/qbic/registration/handler/registries/NfTower.groovy
+++ src/main/groovy/life/qbic/registration/handler/registries/NfTower.groovy
@@ -5,11 +5,9 @@ import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.uri.UriBuilder
 
 /**
- * <class short description - 1 Line!>
+ * <p>Enables basic interaction with a Nextflow Tower API.</p>
  *
- * <More detailed description - When to use, what it solves, etc.>
- *
- * @since <versiontag>
+ * @since 1.0.0
  */
 class NfTower {
 
@@ -17,6 +15,9 @@ class NfTower {
 
     private final String NXF_TOWER_API_TOKEN
 
+    /**
+     * Requires the environment variables NXF_TOWER_API_URL and NXF_TOWER_API_TOKEN to be set.
+     */
     NfTower() {
         NXF_TOWER_API_URL = Objects.requireNonNull(
                 System.getenv("NXF_TOWER_API_URL"),
@@ -26,6 +27,11 @@ class NfTower {
         "The NXF_TOWER_API_TOKEN environment variable was not set.")
     }
 
+    /**
+     * Returns the pipeline Github repo slug for a given Nextflow Tower run identifier.
+     * @param workflowId a Nextflow Tower run id
+     * @return A pipeline repo slug, if found.
+     */
     Optional<String> getPipelineName(String workflowId) {
         RxHttpClient httpClient = RxHttpClient.create(NXF_TOWER_API_URL as URL)
         String query = UriBuilder.of("${NXF_TOWER_API_URL}/workflow/{workflowId}")
