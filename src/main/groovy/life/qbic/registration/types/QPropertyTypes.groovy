6e3bfaa2f738554331b421d615c8dd5fc6e8e808
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-06-23 09:22:29 +0200

    Store workflow run id and nf-core pipeline name

---
+++ src/main/groovy/life/qbic/registration/types/QPropertyTypes.groovy
@@ -0,0 +1,13 @@
+package life.qbic.registration.types
+
+/**
+ * <p>Controlled vocabulary for openBIS property types</p>
+ *
+ * @since 1.0.0
+ */
+enum QPropertyTypes {
+
+    Q_WF_ID,
+    Q_WF_NAME
+
+}
