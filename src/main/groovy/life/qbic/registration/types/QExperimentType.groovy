d4fdf2f4527ee344e7f75c05f1cc87fc5a688281
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-06-24 17:27:53 +0200

    Update openBIS types

--- src/main/groovy/life/qbic/registration/types/QExperimentType.groovy
+++ src/main/groovy/life/qbic/registration/types/QExperimentType.groovy
@@ -7,6 +7,10 @@ package life.qbic.registration.types
  */
 enum QExperimentType {
 
+    Q_WF_NGS_ATAC_SEQ,
+    Q_WF_NGS_BCELL_REPERTOIRE_SEQ,
+    Q_WF_NGS_CHIP_SEQ,
+    Q_WF_NGS_HLATYPING,
     Q_WF_NGS_RNA_EXPRESSION_ANALYSIS,
     Q_WF_NGS_VARIANT_CALLING
 
